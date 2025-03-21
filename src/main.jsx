fe04a6b7c2c384c395cc3c051cd4d083130cf63b
Sanil-Surve <sanilsurve42@gmail.com>
2025-03-21 12:03:20 +0530

    redux persist added

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import '@ant-design/v5-patch-for-react-19';
 import { Provider } from 'react-redux';
+import { persistor } from './store';
+import { PersistGate } from 'redux-persist/integration/react';
 import store from './store';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>,
 )
--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,10 +1,38 @@
+// import { configureStore } from "@reduxjs/toolkit";
+// import mqttReducer from "./mqttSlice";
+
+// export const store = configureStore({
+//   reducer: {
+//     mqtt: mqttReducer,
+//   },
+// });
+
+// export default store;
+
 import { configureStore } from "@reduxjs/toolkit";
 import mqttReducer from "./mqttSlice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // Local Storage
+import { combineReducers } from "redux";
 
-export const store = configureStore({
-  reducer: {
-    mqtt: mqttReducer,
-  },
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const rootReducer = combineReducers({
+  mqtt: mqttReducer,
 });
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+export const persistor = persistStore(store);
 export default store;
